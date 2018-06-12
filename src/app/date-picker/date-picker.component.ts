5956d5f0d5bbb0bda4d720b652d8faa6c7e73d81
Eryk Mariankowski <dagi12@o2.pl>
2018-06-12 13:43:49 +0200

    003988 filtracja, poprawki

--- src/app/date-picker/date-picker.component.ts
+++ src/app/date-picker/date-picker.component.ts
@@ -1,35 +1,52 @@
 import {Component, forwardRef, Input, OnInit} from '@angular/core';
 import {ControlValueAccessor, NG_VALUE_ACCESSOR} from '@angular/forms';
-import {MyNgModel} from '../global/my-ng-model';
+import {BsLocaleService} from 'ngx-bootstrap';
 
 @Component({
   selector: 'app-date-picker',
   templateUrl: './date-picker.component.html',
-  styleUrls: ['./date-picker.component.css'],
+  styleUrls: ['./date-picker.component.scss'],
   providers: [{
     provide: NG_VALUE_ACCESSOR,
     useExisting: forwardRef(() => DatePickerComponent),
     multi: true
   }]
 })
-export class DatePickerComponent implements OnInit, ControlValueAccessor, MyNgModel<any> {
+export class DatePickerComponent implements OnInit, ControlValueAccessor {
   @Input() placeholder: string;
-  model: any;
   onChange;
 
-  constructor() {
+  constructor(private localeService: BsLocaleService) {
+    this.localeService.use('pl');
+  }
+
+  private _model = null;
+
+  get model(): any {
+    return this._model;
+  }
+
+  set model(model: any) {
+    this.writeValue(model);
+    this._model = model;
+  }
+
+  ngOnInit(): void {
   }
 
   writeValue(obj: any): void {
+    if (this.onChange) {
+      this.onChange(obj);
+    }
+    this._model = obj;
   }
 
   registerOnChange(fn: any): void {
+    this.onChange = fn;
   }
 
   registerOnTouched(fn: any): void {
   }
 
-  ngOnInit() {
-  }
 
 }
