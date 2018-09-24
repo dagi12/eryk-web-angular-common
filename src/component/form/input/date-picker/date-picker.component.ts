ea4ed6a5aec1a6bc7ebdeec8e4610964773315ba
Eryk Mariankowski <dagi12@o2.pl>
2018-09-24 13:58:07 +0200

    004238 datepicker jeździ z modalem

--- src/component/form/input/date-picker/date-picker.component.ts
+++ src/component/form/input/date-picker/date-picker.component.ts
@@ -1,6 +1,8 @@
-import {Component, Injector, Input} from '@angular/core';
+import {Component, Injector, Input, NgZone, OnDestroy, OnInit, ViewChild} from '@angular/core';
 import {AbstractValueAccessor, MakeProvider} from '../abstract-value-accessor.component';
 import {newId} from '../../../../util/utils';
+import {BsDatepickerDirective} from 'ngx-bootstrap';
+import 'rxjs/add/observable/fromEvent';
 
 @Component({
   selector: 'app-date-picker',
@@ -8,22 +10,45 @@ import {newId} from '../../../../util/utils';
   styleUrls: ['./date-picker.component.scss'],
   providers: [MakeProvider(DatePickerComponent)]
 })
-export class DatePickerComponent extends AbstractValueAccessor<Date> {
+export class DatePickerComponent extends AbstractValueAccessor<Date> implements OnInit, OnDestroy {
 
+  @ViewChild(BsDatepickerDirective) datepicker: BsDatepickerDirective;
   @Input() placeholder: string;
-  id = newId();
   @Input() label: string;
   @Input() required: boolean;
   @Input() disabled: boolean;
   @Input() submitted: boolean;
-
-  constructor(injector: Injector) {
-    super(injector);
-  }
-
+  id = newId();
+  eventOptions: boolean | { capture?: boolean, passive?: boolean };
   config = {
     required: 'Pole nie może być puste',
     date: 'Data musi być późniejsza od daty dzisiejszej'
   };
+  scroll = (): void => {
+    if (this.datepicker.isOpen) {
+      this.ngZone.run(() => {
+        this.datepicker.hide();
+      });
+    }
+  };
+
+  constructor(injector: Injector, private ngZone: NgZone) {
+    super(injector);
+  }
+
+  ngOnInit() {
+    super.ngOnInit();
+    this.eventOptions = {
+      capture: true,
+      passive: true
+    };
+    this.ngZone.runOutsideAngular(() => {
+      window.addEventListener('scroll', this.scroll, <any>this.eventOptions);
+    });
+  }
+
+  ngOnDestroy() {
+    window.removeEventListener('scroll', this.scroll, <any>this.eventOptions);
+  }
 
 }
