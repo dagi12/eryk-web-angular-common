efda1d9439d27eec2589062309ef938900637cdb
Eryk Mariankowski <dagi12@o2.pl>
2019-02-20 14:02:59 +0100

    004582 poprawki stylowania

--- src/component/modal/header-modal/header-modal.component.ts
+++ src/component/modal/header-modal/header-modal.component.ts
@@ -1,19 +1,20 @@
 import {Component, EventEmitter, Input, Output} from '@angular/core';
 
 @Component({
-    selector: 'app-header-modal',
-    templateUrl: './header-modal.component.html',
-    styles: []
+  selector: 'app-header-modal',
+  templateUrl: './header-modal.component.html',
+  styles: []
 })
 export class HeaderModalComponent {
 
-    @Input() modalTitle: string;
-    @Input() helpContent: string;
-    @Output() close = new EventEmitter();
+  @Input() icon = 'fa-info';
+  @Input() modalTitle: string;
+  @Input() helpContent: string;
+  @Output() close = new EventEmitter();
 
 
-    locClose() {
-        return this.close.emit();
-    }
+  locClose() {
+    return this.close.emit();
+  }
 
 }
--- src/component/modal/modal-header/modal-header.component.ts
+++ src/component/modal/modal-header/modal-header.component.ts
@@ -3,13 +3,14 @@ import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
 @Component({
   selector: 'app-modal-header',
   templateUrl: './modal-header.component.html',
-    styleUrls: ['./modal-header.component.css']
+  styleUrls: ['./modal-header.component.css']
 })
 export class ModalHeaderComponent implements OnInit {
 
   @Output() close = new EventEmitter();
   @Input() helpContent: string;
   @Input() modalTitle: string;
+  @Input() icon = 'fa-info';
 
   constructor() {
   }
